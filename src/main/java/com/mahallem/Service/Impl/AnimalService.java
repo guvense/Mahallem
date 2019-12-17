5a057142fa1942aef369e77e66b52564699f772c
sercankal <sercankaliu@gmail.com>
2019-12-17 23:33:51 +0300

    Animal End Points is created

--- src/main/java/com/mahallem/Service/Impl/AnimalService.java
+++ src/main/java/com/mahallem/Service/Impl/AnimalService.java
@@ -3,22 +3,17 @@ package com.mahallem.Service.Impl;
 import com.mahallem.DTO.Request.AnimalRequest;
 import com.mahallem.DTO.Response.AnimalResponse;
 import com.mahallem.Entity.Animal;
-import com.mahallem.Entity.House;
-import com.mahallem.Entity.User;
 import com.mahallem.Exception.AnimalNotFoundException;
-import com.mahallem.Exception.UserNotFoundException;
 import com.mahallem.Repository.AnimalRepository;
 import com.mahallem.Repository.HouseRepository;
 import com.mahallem.Repository.UserRepository;
 import com.mahallem.Service.IAnimalService;
 import lombok.RequiredArgsConstructor;
-import org.bson.types.ObjectId;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
 import java.util.Optional;
-import java.util.OptionalInt;
 
 @Service
 @RequiredArgsConstructor
@@ -29,11 +24,6 @@ public class AnimalService implements IAnimalService {
     @NotNull
     private final AnimalRepository animalRepository;
 
-    @NotNull
-    private final UserRepository userRepository;
-
-    @NotNull
-    private final HouseRepository houseRepository;
 
     @Override
     public AnimalResponse saveAnimal(String id, AnimalRequest animalRequest) {
