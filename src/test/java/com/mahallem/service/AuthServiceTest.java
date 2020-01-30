b5a97379131b44758971d0163b8f9101ccd10a09
srcnkl <sercankaliu@gmail.com>
2020-01-30 21:24:38 +0300

    test case is development for loginUser metod

--- src/test/java/com/mahallem/service/AuthServiceTest.java
+++ src/test/java/com/mahallem/service/AuthServiceTest.java
@@ -3,6 +3,7 @@ package com.mahallem.service;
 import com.mahallem.dto.Request.AuthRequest;
 import com.mahallem.dto.Response.AuthResponse;
 import com.mahallem.entity.User;
+import com.mahallem.exception.UserOrPasswordWrongException;
 import com.mahallem.exception.UsernameExistException;
 import com.mahallem.repository.Impl.UserRepositoryImpl;
 import com.mahallem.resource.AuthResource;
@@ -11,28 +12,23 @@ import com.mahallem.util.JwtUtil;
 import org.bson.types.ObjectId;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.runner.RunWith;
-import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doNothing;
 
 
 // MethodName_StateUnderTest_ExpectedBehavior
@@ -60,16 +56,22 @@ public class AuthServiceTest {
 
     private User u;
 
+    private String password;
+
     @Before
     public void init() {
 
         u = new User();
         u.setUserName("test");
 
+        this.password="12345";
+
         this.authRequest = AuthResource.authRequest;
 
         when(userRepository.save(any())).thenReturn(u);
         when(bCryptPasswordEncoder.encode(anyString())).thenReturn("12345");
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.of(u));
+        when(bCryptPasswordEncoder.matches(any(), any())).thenReturn(true);
 
     }
 
@@ -103,4 +105,32 @@ public class AuthServiceTest {
 
 
     }
+
+    @Test(expected = UserOrPasswordWrongException.class)
+    public void loginUser_UserNameWrong_ExceptionThrown() {
+
+        when(userRepository.findByUserName(anyString())).thenReturn(Optional.empty());
+        AuthResponse authResponse = authService.loginUser(u.getUserName(),password);
+        assertNull(authResponse.getToken());
+    }
+
+
+    @Test(expected = UserOrPasswordWrongException.class)
+    public void loginUser_PasswordWrong_ExceptionThrown() {
+        when(bCryptPasswordEncoder.matches(any(), any())).thenReturn(false);
+        AuthResponse authResponse = authService.loginUser(u.getUserName(),password);
+        assertNull(authResponse.getToken());
+    }
+
+
+    @Test
+    public void loginUser_LoginUser_LoginIsSuccessfully() {
+        AuthResponse authResponse = authService.loginUser(u.getUserName(),password);
+        assertNotNull(authResponse.getToken());
+        assertNotNull(authResponse.getFirstName());
+        assertNotNull(authResponse.getLastName());
+        assertNotNull(authResponse.getUserName());
+    }
+
+
 }
