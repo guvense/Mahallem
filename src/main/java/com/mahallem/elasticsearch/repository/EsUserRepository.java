c94caa592ec00bc2e13537afcac171f9e7586d89
guvense <guven.seckin.4@gmail.com>
2020-04-25 19:39:01 +0300

    es fix

--- src/main/java/com/mahallem/elasticsearch/repository/EsUserRepository.java
+++ src/main/java/com/mahallem/elasticsearch/repository/EsUserRepository.java
@@ -3,8 +3,9 @@ package com.mahallem.elasticsearch.repository;
 import com.mahallem.elasticsearch.model.RegisteredUser;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface EsUserRepository extends ElasticsearchRepository<RegisteredUser, String> {
 
 }
