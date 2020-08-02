d8b76e848244da839acf9c6b00a093401ad1331d
GokhanYilmaz44 <yilmazgokhan.1992@gmail.com>
2020-08-02 23:02:57 +0300

    Implement blob storage

---
+++ src/main/java/com/mahallem/storage/BlobController.java
@@ -0,0 +1,35 @@
+package com.mahallem.storage;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
+@RestController
+@RequestMapping("blob")
+public class BlobController {
+
+    @Value("${blob}")
+    private Resource blobFile;
+
+    @GetMapping
+    public String readBlobFile() throws IOException{
+        return StreamUtils.copyToString(
+                this.blobFile.getInputStream(),
+                Charset.defaultCharset()
+        );
+    }
+
+    @PostMapping
+    public String writeBlobFile(@RequestBody String data) throws IOException{
+        try (OutputStream os = ((WritableResource) this.blobFile).getOutputStream()){
+            os.write(data.getBytes());
+        }
+        return "file was updated";
+    }
+}
---
+++ src/main/java/com/mahallem/storage/BlobStorageService.java
@@ -0,0 +1,7 @@
+package com.mahallem.storage;
+
+public interface BlobStorageService {
+
+    public String addProfilePicture(String base64);
+
+}
