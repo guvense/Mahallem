508bcab8f0bfc4ea27884c924c9dbb5f8cb0ba48
guvense <guven.seckin.4@gmail.com>
2020-08-05 22:27:34 +0300

    Add logger

--- src/main/java/com/mahallem/blobstorage/BlobStorageServiceImpl.java
+++ src/main/java/com/mahallem/blobstorage/BlobStorageServiceImpl.java
@@ -2,11 +2,14 @@ package com.mahallem.blobstorage;
 
 import com.mahallem.exception.UploadPictureFailedException;
 import com.mahallem.blobstorage.BlobStorageService;
+import com.mahallem.service.impl.AuthServiceImpl;
 import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,11 +25,15 @@ import java.util.UUID;
 @ConditionalOnProperty(value = "mock.azure.blobstorage.service.enabled", havingValue = "false", matchIfMissing = true)
 public class BlobStorageServiceImpl implements BlobStorageService {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(BlobStorageServiceImpl.class);
+
     private final CloudBlobContainer cloudBlobContainer;
 
     @Override
     public URI uploadPicture(MultipartFile multipartFile) {
         URI uri;
+        LOGGER.info("Profile image uploading, image name: {}", multipartFile.getName());
+
         try {
             String extension = FilenameUtils.getExtension(multipartFile.getOriginalFilename());
             String fileName = String.join(".", UUID.randomUUID().toString(), extension);
